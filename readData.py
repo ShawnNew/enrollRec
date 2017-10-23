a67b3d822580d23a3b642ddbb26c46f3f9fd5d6d
ShawnNew <ncxinhanzhong@gmail.com>
2017-10-22 20:58:54 -0400

    Add the code to read csv file

--- readData.py
+++ readData.py
@@ -1,7 +1,23 @@
 import json
+import csv
 import pprint
 
-path = "/Users/apple/Desktop/CEE/enrollmentData.json"
-file = open(path , 'r')
-enrollmentData = json.load(file)
-pprint.pprint (enrollmentData[0])
+path1 = "/Users/apple/Desktop/enrollmentData1.json"
+path2 = "/Users/apple/Desktop/stuDataOfTenSchools.csv"
+
+file2 = open(path2).read().decode('utf8')
+reader = csv.DictReader(file2)
+dict_list = []
+for line in reader:
+    dict_list.append(line)
+
+pprint.pprint(dict_list)
+
+#enrollmentData2 = csv_dict_list(file2)
+#pprint.pprint(enrollmentData2)
+
+
+file1 = open(path1 , 'r')
+enrollmentData1 = json.load(file1)
+
+#pprint.pprint (enrollmentData[0])
